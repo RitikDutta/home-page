7ca0e01b71d917cd8a04630c6bfca86bbf55661d
ritik dutta <ritikduttagd@gmail.com>
2025-03-29 19:14:06 +0530

    added in config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useRef, useEffect, useState, useCallback } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/home-page/',
   server: {
     host: true,
     allowedHosts: ['49ef-2401-4900-8819-f8b-29a-8f7a-6b4a-36be.ngrok-free.app'], // or 'all' for all hosts
