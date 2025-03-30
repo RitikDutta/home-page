27a81168f169e35391c43b16aff497c2282f4611
ritik dutta <ritikduttagd@gmail.com>
2025-03-30 07:33:10 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/home-page/', // 👈 MUST match your GitHub repo name
   plugins: [react()],
-  base: '/home-page/',
-  server: {
-    host: true,
-    allowedHosts: ['49ef-2401-4900-8819-f8b-29a-8f7a-6b4a-36be.ngrok-free.app'], // or 'all' for all hosts
-  },
 })
