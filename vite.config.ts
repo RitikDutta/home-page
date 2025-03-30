bcd1bf7302dc4043a8a6ca6505caa2da6176170b
ritik dutta <ritikduttagd@gmail.com>
2025-03-30 07:58:12 +0530

    changed base to root

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/home-page/', // 👈 MUST match your GitHub repo name
+  base: '/', // 👈 MUST match your GitHub repo name
   plugins: [react()],
 })
